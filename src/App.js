ea234169fd6771290554074852de94c48f3e6b78
Sahil Ghauri <sghauri@albany.edu>
2023-03-16 11:59:47 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {useState} from "react";
 
 function App() {
+  const [word, setWord] = useState("");
+  // const API_URL = "https://api.datamuse.com/words?rel_syn=";
+  const API_URL = "http://127.0.0.1:8080/words?rel_syn=";
+  const [result, setResult] = useState([]);
+
+  function handleChange(e) {
+    setWord(e.target.value);
+  }
+
+  function fetchSynonyms(input) {
+    setWord(input);
+    const full_url = API_URL + input;
+    console.log(full_url);
+    fetch(full_url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data); // Prints result from `response.json()` in getRequest
+
+        setResult(data);
+      })
+      .catch((error) => console.error(error));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetchSynonyms(word);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="entryWord">Enter Word</label>
+          <input id="entryWord" onChange={handleChange} value={word}></input>
+          <button type="submit" onClick={handleSubmit}>
+            Submit
+          </button>
+          <br></br>
+          <br></br>
+          <br></br>
+          {result.length !== 0 ? (
+            <div>
+              <label>The results in order are as follows</label>
+              <ol>
+                {result.map((i, idx) => (
+                  <li
+                    key={idx}
+                    onClick={() => {
+                      return fetchSynonyms(i.word);
+                    }}
+                  >
+                    {i.word}
+                  </li>
+                ))}
+              </ol>
+            </div>
+          ) : (
+            ""
+          )}
+        </form>
+      </div>
     </div>
   );
 }
